1f38f5d86087594059b1357a2b91879bff7187a1
code-architect <codearchitectdeveloper@gmail.com>
2016-04-26 03:32:16 +0530

    checking if the absolute path is not defined then exit

--- facebook-footer-link.php
+++ facebook-footer-link.php
@@ -3,9 +3,15 @@
 /*
 Plugin Name: Code Architect Facebook Footer Link
 Plugin URI: http://codearchitect.in/
-Description: A brief description of the Plugin.
+Description: Add a Facebook profile page link to the end of the posts
 Version: 1.0
 Author: Code-Architect
 Author URI: http://codearchitect.in/
 License: GPL2
 */
+
+// Exit if accessed directly
+if(!defined('ABSPATH'))
+{
+    exit;
+}

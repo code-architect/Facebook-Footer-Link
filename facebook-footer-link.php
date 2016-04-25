609a78d5ba21459cef76918b0c5634081710e1e2
code-architect <codearchitectdeveloper@gmail.com>
2016-04-26 03:24:33 +0530

    created the basic structure of the plugin

---
+++ facebook-footer-link.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Code Architect Facebook Footer Link
+Plugin URI: http://codearchitect.in/
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Code-Architect
+Author URI: http://codearchitect.in/
+License: GPL2
+*/

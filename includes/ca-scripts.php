14ca98649661a67e2bbdf206fbd3a6306ef1e880
code-architect <codearchitectdeveloper@gmail.com>
2016-04-27 16:52:28 +0530

    using add_action method to add them (.js, .css)

--- includes/ca-scripts.php
+++ includes/ca-scripts.php
@@ -6,3 +6,5 @@ function ca_add_script_n_style()
     wp_enqueue_style('ca-main-style', plugins_url().'/facebook-footer-link/css/ca_style.css');
     wp_enqueue_script('ca-main-script', plugins_url().'/facebook-footer-link/js/ca_main.js');
 }
+
+add_action('wp_enqueue_scripts', 'ca_add_script_n_style');
